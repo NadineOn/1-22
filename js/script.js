d089f82e6fecdef4570fc7e77d5c4083cb5dcd6f
nonishenko <onishenko@ebola.com.ua>
2015-03-25 20:20:00 +0200

    index.html

--- js/script.js
+++ js/script.js
@@ -1,80 +1,63 @@
 var AppNS = {};
-var windowHeight, tooltipHeight, tooltipContentHeight;
+var windowHeight, tooltipHeight, tooltipContentHeight, infoBlockHeight;
 (function($, undefined){
 
     /**
      * Init events
      */
     AppNS.init = function() {
-        //
-        //AppNS.makeOrder();
-
-        //if($('#slider').length) {
-        //    AppNS.mainSlider();
-        //}
-
-        //if($('.b-info').length) {
-        //    AppNS.slideHoverInfoBlock();
-        //    AppNS.clickHoverInfoBlock();
-        //}
 
         $(window).resize(function(){
             windowHeight = $(window).height();
             tooltipHeight = Math.round(windowHeight*0.7);
-            tooltipContentHeight = windowHeight - tooltipHeight; // 25px - is a wave height
+            tooltipContentHeight = windowHeight - tooltipHeight;
+            infoBlockHeight = $('.b-page__item').outerHeight() + 25;// 25px - is a wave height
         }).resize();
 
         AppNS.setCookie(); // for welcome popup
 
         AppNS.showMenu();
+        AppNS.showMainInfo();
 
-        //var windowHeight = $(window).height();
-
-        //var tooltipHeight = Math.round(windowHeight*0.7);
-        //var tooltipContentHeight = windowHeight - tooltipHeight; // 25px - is a wave height
         if (!AppNS.isTouchDevice()) {
             $('.b-page__item').hover(function(){
-                $(this).find('.b-info').stop().animate({
+                console.log(infoBlockHeight)
+                var $infoBlock = $(this).find('.b-info');
+                $infoBlock.stop().animate({
                     top: tooltipHeight+'px'
                 }, function() {
-                    $('.b-info').css('height', tooltipContentHeight)
+                    $infoBlock.css('height', tooltipContentHeight)
                 })
             }, function() {
-                $(this).find('.b-info').stop().animate({
-                    top: '120%'
+                var $infoBlock = $(this).find('.b-info');
+                $infoBlock.stop().animate({
+                    top: infoBlockHeight+'px'
                 }, function() {
-                    $('.b-info').css('height', 'auto')
+                    $infoBlock.removeClass('b-info_open').css('height', 'auto')
                 })
             });
         } else {
-            $('.b-page__item').click(function(){
+           /* $('.b-page__item').click(function(){
                 $(this).find('.b-info').stop().animate({
                     top: '70%'
                 })
-            }/*, function() {
+            }, function() {
                 $(this).find('.b-info').stop().animate({
                     bottom: '-400px'
                 })
-            }*/);
+            });*/
         }
-
-
-//        function is_touch_device() {
-//            return (('ontouchstart' in window)
-//                || (navigator.MaxTouchPoints > 0)
-//                || (navigator.msMaxTouchPoints > 0));
-//        }
-
-
-
-        //$('.b-page__item').toggle(function(){
-        //    console.log('111')
-        //    //$(this).bind('mouseover');
-        //}, function() {
-        //    console.log('333')
-        //    //$(this).bind('mouseleave');
-        //})
     };
+
+    AppNS.showMainInfo = function() {
+        $('.b-info').css('top', infoBlockHeight+'px')
+        $('.b-info__more').on('click', function(){
+            var $infoBlock = $(this).closest('.b-info');
+            $infoBlock.addClass('b-info_open').stop().animate({
+                top: '7%'
+            });
+        })
+    }
 
     AppNS.isTouchDevice = function() {
         return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
@@ -127,53 +110,6 @@ var windowHeight, tooltipHeight, tooltipContentHeight;
         }
         return c_value;
     }
-    //AppNS.resizer = function() {
-    //    $(window).resize(function(){
-    //        $menu.removeClass('main-panel_visible');
-    //    })
-    //}
-
-
-    //AppNS.mainSlider = function() {
-    //    var slider = $('#slider .bxslider').bxSlider({
-    //        minSlides: 1,
-    //        maxSlides: 1,
-    //        slideWidth: '999',
-    //        mode: 'fade',
-    //        pager: false,
-    //        controls: false,
-    //        auto: true,
-    //        speed: 800
-    //    });
-    //
-    //    $('#slider .next').click(function(){
-    //        slider.goToNextSlide();
-    //    });
-    //    $('#slider .prev').click(function(){
-    //        slider.goToPrevSlide();
-    //    });
-    //}
-
-    ////var $infoBlock = $('.b-info');
-    //AppNS.showInfoBlock = function($el) {
-    //    $el.stop().animate({
-    //        bottom: 0
-    //    })
-    //}
-    //
-    //AppNS.hideInfoBlock = function($el) {
-    //    $el.stop().animate({
-    //        bottom: '-400px'
-    //    })
-    //}
-
-    //AppNS.makeOrder = function() {
-    //    $('.make-order').fancybox({
-    //        padding : 0,
-    //        margin: 35,
-    //        minWidth:500
-    //    })
-    //}
 
     // call to init events
     $(AppNS.init);
